86fae1f4a84d8e240a5071083ecc821fe8826ee6
Franco Lippi <soyflippi@gmail.com>
2021-05-03 22:06:33 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -23,8 +23,20 @@ $app->addErrorMiddleware(true, true, true);
 
 // Enable CORS
 $app->add(function (Request $request, RequestHandlerInterface $handler): Response {
+    $routeContext = RouteContext::fromRequest($request);
+    $routingResults = $routeContext->getRoutingResults();
+    $methods = $routingResults->getAllowedMethods();
+    $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
     $response = $handler->handle($request);
+
     $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+    $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+    // Optional: Allow Ajax CORS requests with Authorization header
+    // $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
     return $response;
 });
 
