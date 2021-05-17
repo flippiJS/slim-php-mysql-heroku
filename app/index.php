cac8fbb58447a80b2917a5ad7b8eaaa4dbcda322
flippi <soyflippi@gmail.com>
2021-05-17 17:05:02 -0300

    add jwt

--- app/index.php
+++ app/index.php
@@ -12,7 +12,7 @@ use Slim\Routing\RouteContext;
 require __DIR__ . '/../vendor/autoload.php';
 
 require_once './db/AccesoDatos.php';
-// require_once './middlewares/Logger.php';
+require_once './middlewares/AutentificadorJWT.php';
 
 require_once './controllers/UsuarioController.php';
 
@@ -29,15 +29,87 @@ $app->addErrorMiddleware(true, true, true);
 
 // Routes
 $app->group('/usuarios', function (RouteCollectorProxy $group) {
-    $group->get('[/]', \UsuarioController::class . ':TraerTodos');
-    $group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
-    $group->post('[/]', \UsuarioController::class . ':CargarUno');
+  $group->get('[/]', \UsuarioController::class . ':TraerTodos');
+  $group->get('/{usuario}', \UsuarioController::class . ':TraerUno');
+  $group->post('[/]', \UsuarioController::class . ':CargarUno');
+});
+
+// JWT test routes
+$app->group('/jwt', function (RouteCollectorProxy $group) {
+
+  $group->post('/crearToken', function (Request $request, Response $response) {    
+    $parametros = $request->getParsedBody();
+
+    $usuario = $parametros['usuario'];
+    $perfil = $parametros['perfil'];
+    $alias = $parametros['alias'];
+
+    $datos = array('usuario' => $usuario, 'perfil' => $perfil, 'alias' => $alias);
+
+    $token = AutentificadorJWT::CrearToken($datos);
+    $payload = json_encode(array('jwt' => $token));
+
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
   });
 
-$app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("Slim Framework 4 PHP");
-    return $response;
+  $group->post('/devolverPayLoad', function (Request $request, Response $response) {
+    $parametros = $request->getParsedBody();
+    $token = $parametros['token'];
 
+    try {
+      $payload = json_encode(array('payload' => AutentificadorJWT::ObtenerPayLoad($token)));
+    } catch (Exception $e) {
+      $payload = json_encode(array('error' => $e->getMessage()));
+    }
+
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
+  });
+
+  $group->post('/devolverDatos', function (Request $request, Response $response) {
+    $parametros = $request->getParsedBody();
+    $token = $parametros['token'];
+
+    try {
+      $payload = json_encode(array('datos' => AutentificadorJWT::ObtenerData($token)));
+    } catch (Exception $e) {
+      $payload = json_encode(array('error' => $e->getMessage()));
+    }
+
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
+  });
+
+  $group->post('/verificarToken', function (Request $request, Response $response) {
+    $parametros = $request->getParsedBody();
+    $token = $parametros['token'];
+    $esValido = false;
+
+    try {
+      AutentificadorJWT::verificarToken($token);
+      $esValido = true;
+    } catch (Exception $e) {
+      $payload = json_encode(array('error' => $e->getMessage()));
+    }
+
+    if ($esValido) {
+      $payload = json_encode(array('valid' => $esValido));
+    }
+
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
+  });
+});
+
+
+$app->get('[/]', function (Request $request, Response $response) {
+  $response->getBody()->write("Slim Framework 4 PHP");
+  return $response;
 });
 
 $app->run();
---
+++ app/middlewares/AutentificadorJWT.php
@@ -0,0 +1,81 @@
+<?php
+
+use Firebase\JWT\JWT;
+
+class AutentificadorJWT
+{
+    private static $claveSecreta = 'T3sT$JWT';
+    private static $tipoEncriptacion = ['HS256'];
+
+    public static function CrearToken($datos)
+    {
+        $ahora = time();
+        $payload = array(
+            'iat' => $ahora,
+            'exp' => $ahora + (60000),
+            'aud' => self::Aud(),
+            'data' => $datos,
+            'app' => "Test JWT"
+        );
+        return JWT::encode($payload, self::$claveSecreta);
+    }
+
+    public static function VerificarToken($token)
+    {
+        if (empty($token)) {
+            throw new Exception("El token esta vacio.");
+        }
+        try {
+            $decodificado = JWT::decode(
+                $token,
+                self::$claveSecreta,
+                self::$tipoEncriptacion
+            );
+        } catch (Exception $e) {
+            throw $e;
+        }
+        if ($decodificado->aud !== self::Aud()) {
+            throw new Exception("No es el usuario valido");
+        }
+    }
+
+
+    public static function ObtenerPayLoad($token)
+    {
+        if (empty($token)) {
+            throw new Exception("El token esta vacio.");
+        }
+        return JWT::decode(
+            $token,
+            self::$claveSecreta,
+            self::$tipoEncriptacion
+        );
+    }
+
+    public static function ObtenerData($token)
+    {
+        return JWT::decode(
+            $token,
+            self::$claveSecreta,
+            self::$tipoEncriptacion
+        )->data;
+    }
+
+    private static function Aud()
+    {
+        $aud = '';
+
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $aud = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $aud = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $aud = $_SERVER['REMOTE_ADDR'];
+        }
+
+        $aud .= @$_SERVER['HTTP_USER_AGENT'];
+        $aud .= gethostname();
+
+        return sha1($aud);
+    }
+}
