184e4fc4a276165f877b14cd30ff7fb247aa9df1
Franco Lippi <soyflippi@gmail.com>
2022-10-31 19:19:07 -0300

    Update Logger.php

--- app/middlewares/Logger.php
+++ app/middlewares/Logger.php
@@ -1,8 +1,8 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Response;
 
 class LoggerMiddleware
 {
