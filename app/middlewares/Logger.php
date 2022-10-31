5b56ef35cc2957086a1fdcdcf8265329e9441f12
Franco Lippi <soyflippi@gmail.com>
2022-10-31 19:26:14 -0300

    Update Logger.php

--- app/middlewares/Logger.php
+++ app/middlewares/Logger.php
@@ -16,11 +16,12 @@ class LoggerMiddleware
      */
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
+        
         $response = $handler->handle($request);
         $existingContent = (string) $response->getBody();
     
         $response = new Response();
-        $response->getBody()->write('Hora:'. date('Y-m-d H:i:s') . ' - ' . $existingContent);
+        $response->getBody()->write('Fecha:'. date('Y-m-d H:i:s') . ' -- Metodo: ' . $request->getMethod() . " -- URL: " . $request->getUri() . " -- Respuesta:" . $existingContent);
     
         return $response;
     }
