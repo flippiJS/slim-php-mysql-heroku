35faf3cc5bb084a93c28e2f5aaa736d8d1142c78
Franco Lippi <soyflippi@gmail.com>
2022-10-31 20:39:18 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -38,9 +38,10 @@ $app->group('/usuarios', function (RouteCollectorProxy $group) {
   });
 
 $app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("Slim Framework 4 PHP");
-    return $response;
-
+    $payload = json_encode(array("mensaje" => "Slim Framework 4 PHP"));
+    
+    $response->getBody()->write($payload);
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
