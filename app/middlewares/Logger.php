16b6f91a37b87ee1a42e29475c3fb1f7855cd795
Franco Lippi <soyflippi@gmail.com>
2022-10-31 19:14:35 -0300

    Update Logger.php

--- app/middlewares/Logger.php
+++ app/middlewares/Logger.php
@@ -1,10 +1,25 @@
-<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-class Logger
+class LoggerMiddleware
 {
-    public static function LogOperacion($request, $response, $next)
+    /**
+     * Example middleware invokable class
+     *
+     * @param  ServerRequest  $request PSR-7 request
+     * @param  RequestHandler $handler PSR-15 request handler
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, RequestHandler $handler): Response
     {
-        $retorno = $next($request, $response);
-        return $retorno;
+        $response = $handler->handle($request);
+        $existingContent = (string) $response->getBody();
+    
+        $response = new Response();
+        $response->getBody()->write('Hora:'. date('Y-m-d H:i:s') . ' - ' . $existingContent);
+    
+        return $response;
     }
 }
